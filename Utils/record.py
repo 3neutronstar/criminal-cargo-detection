bfa98a8d07e656113fb913ac6e181b4b6489adba
3neutronstar <minsoo.kang0918@gmail.com>
2021-06-29 14:58:26 +0900

    add the dh

--- Utils/record.py
+++ Utils/record.py
@@ -4,6 +4,13 @@ import numpy as np
 
 class record_data:
     def __init__(self,data_path,save_path,configs):
+        test_csv=pd.read_csv(data_path,'train.csv')
 
-    
+
     def run(self):
+
+        return
+
+    def _record(self):
+        
+        return
