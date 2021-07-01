e5eb40bb7f1478ec1408d78bbbc69f4ef3048403
3neutronstar <minsoo.kang0918@gmail.com>
2021-07-01 18:13:09 +0900

    preprocessing mapping dictionary

--- DataProcessing/preprocessing.py
+++ DataProcessing/preprocessing.py
@@ -8,7 +8,7 @@ class Preprocessing:
         if os.path.exists(os.path.join(data_path,'mapping.json'))==False:
             train_csv=pd.read_csv(os.path.join(data_path,'train.csv'))
             test_csv=pd.read_csv(os.path.join(data_path,'test.csv'))
-            mapping_dict=MappingJsonGenerator(train_csv,test_csv)
+            mapping_dict=MappingJsonGenerator(train_csv,test_csv,'Missing', ['신고번호', '신고일자', '신고중량(KG)', '과세가격원화금액', '관세율', '검사결과코드'])()
             self.mapping_dict=mapping_dict.forward()
 
     
