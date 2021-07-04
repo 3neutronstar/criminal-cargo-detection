1541bbbd31097c76abd134b05f230e60691afa78
3neutronstar <minsoo.kang0918@gmail.com>
2021-07-04 17:05:50 +0900

    주석처리 hs code

--- DataProcessing/make_dict.py
+++ DataProcessing/make_dict.py
@@ -10,17 +10,17 @@ class MappingJsonGenerator():
         self.train_csv = train_csv.drop(['우범여부', '핵심적발'] + self.drop_list, axis = 1)
         self.test_csv = test_csv.drop(self.drop_list, axis = 1)
         
-        train_hs_code = np.array(self.train_csv['HS10단위부호'], dtype = str)
-        test_hs_code = np.array(self.test_csv['HS10단위부호'], dtype = str)
+        # train_hs_code = np.array(self.train_csv['HS10단위부호'], dtype = str)
+        # test_hs_code = np.array(self.test_csv['HS10단위부호'], dtype = str)
         
-        self.train_csv = self.train_csv.drop(['HS10단위부호'], axis = 1)
-        self.test_csv = self.test_csv.drop(['HS10단위부호'], axis = 1)
+        # self.train_csv = self.train_csv.drop(['HS10단위부호'], axis = 1)
+        # self.test_csv = self.test_csv.drop(['HS10단위부호'], axis = 1)
         
-        self.train_hs_upper_code = np.array([s[:2] for s in train_hs_code]).reshape(-1, 1)
-        self.test_hs_upper_code = np.array([s[:2] for s in test_hs_code]).reshape(-1, 1)
+        # self.train_hs_upper_code = np.array([s[:2] for s in train_hs_code]).reshape(-1, 1)
+        # self.test_hs_upper_code = np.array([s[:2] for s in test_hs_code]).reshape(-1, 1)
 
-        self.train_hs_middle_code = np.array([s[2:4] for s in train_hs_code]).reshape(-1, 1)
-        self.test_hs_middle_code = np.array([s[2:4] for s in test_hs_code]).reshape(-1, 1)
+        # self.train_hs_middle_code = np.array([s[2:4] for s in train_hs_code]).reshape(-1, 1)
+        # self.test_hs_middle_code = np.array([s[2:4] for s in test_hs_code]).reshape(-1, 1)
 
         self.train_csv = self.train_csv.fillna(self.fillna_str)
         self.test_csv = self.test_csv.fillna(self.fillna_str)
@@ -35,8 +35,8 @@ class MappingJsonGenerator():
     def forward(self):
         
         train_np, test_np = np.array(self.train_csv, dtype = str), np.array(self.test_csv, dtype = str)
-        train_np = np.concatenate([train_np, self.train_hs_upper_code, self.train_hs_middle_code], axis = 1)
-        test_np = np.concatenate([test_np, self.test_hs_upper_code, self.test_hs_middle_code], axis = 1)
+        # train_np = np.concatenate([train_np, self.train_hs_upper_code, self.train_hs_middle_code], axis = 1)
+        # test_np = np.concatenate([test_np, self.test_hs_upper_code, self.test_hs_middle_code], axis = 1)
         
         for i in range(len(self.column_list)):
             
