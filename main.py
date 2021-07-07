0bbff3da5ed259e3cea4ce3992219ba2208d67ef
3neutronstar <minsoo.kang0918@gmail.com>
2021-07-07 13:04:15 +0900

    add sj mode

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def parse_args(args):
         epilog="python run.py mode")
     
     parser.add_argument(
-        'mode', type=str,choices=['gen_data','train_crime','train_priority','train_mixed','eval','record','train_xgboost_crime','train_xgboost_priority'])
+        'mode', type=str,choices=['gen_data','train_crime','train_priority','train_mixed','sj','record','train_xgboost_crime','train_xgboost_priority'])
     #TRAIN SECTION
     parser.add_argument(
         '--seed', type=int, default=1,
