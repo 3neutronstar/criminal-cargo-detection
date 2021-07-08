e327efc31f052ae7903679fd254261b801880d0e
ykykyk112 <ykykyk112@naver.com>
2021-07-08 15:36:06 +0900

    2021.07.08 Add new masking algorithm applied dictionary
    * Drop '검사결과코드'
    * Add drop list on make_dict

--- DataProcessing/make_dict.py
+++ DataProcessing/make_dict.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 class MappingJsonGenerator():
     def __init__(self, train_csv, test_csv, fillna_str, drop_list):
@@ -7,25 +8,25 @@ class MappingJsonGenerator():
         self.drop_list = drop_list
         self.crime = np.array(train_csv['우범여부'])
         self.train_csv = train_csv.drop(['우범여부', '핵심적발'] + self.drop_list, axis = 1)
-        self.test_csv = test_csv.drop(self.drop_list, axis = 1,error='ignore')
+        self.test_csv = test_csv.drop(self.drop_list, axis = 1)
         
-        train_hs_code = np.array(self.train_csv['HS10단위부호'], dtype = str)
-        test_hs_code = np.array(self.test_csv['HS10단위부호'], dtype = str)
-        
+        # Split HS10-code to HS_upper and HS_middle
+        self.train_csv['HS_upper'] = self.train_csv['HS10단위부호'].astype(str).str.slice(start = 0, stop = 2)
+        self.train_csv['HS_middle'] = self.train_csv['HS10단위부호'].astype(str).str.slice(start = 2, stop = 4)
+
+        self.test_csv['HS_upper'] = self.test_csv['HS10단위부호'].astype(str).str.slice(start = 0, stop = 2)
+        self.test_csv['HS_middle'] = self.test_csv['HS10단위부호'].astype(str).str.slice(start = 2, stop = 4)
+
         self.train_csv = self.train_csv.drop(['HS10단위부호'], axis = 1)
         self.test_csv = self.test_csv.drop(['HS10단위부호'], axis = 1)
-        
-        self.train_hs_upper_code = np.array([s[:2] for s in train_hs_code]).reshape(-1, 1)
-        self.test_hs_upper_code = np.array([s[:2] for s in test_hs_code]).reshape(-1, 1)
 
-        self.train_hs_middle_code = np.array([s[2:4] for s in train_hs_code]).reshape(-1, 1)
-        self.test_hs_middle_code = np.array([s[2:4] for s in test_hs_code]).reshape(-1, 1)
-
+        # Filling nan to 'Missing'
         self.train_csv = self.train_csv.fillna(self.fillna_str)
         self.test_csv = self.test_csv.fillna(self.fillna_str)
-        self.column_list = np.array(np.concatenate([self.train_csv.columns, np.array(['HS_upper', 'HS_middle'], dtype = str)], axis = 0), dtype=str)
+
         self.column_list = np.array(self.train_csv.columns, dtype=str)
         self.crime_idx = np.where(self.crime == 1)[0]
+        self.crime_proportion = self.crime_idx.shape[0]/self.crime.shape[0]
         self.dictionary = dict()
         
     def __call__(self):
@@ -34,30 +35,65 @@ class MappingJsonGenerator():
     def forward(self):
         
         train_np, test_np = np.array(self.train_csv, dtype = str), np.array(self.test_csv, dtype = str)
-        train_np = np.concatenate([train_np, self.train_hs_upper_code, self.train_hs_middle_code], axis = 1)
-        test_np = np.concatenate([test_np, self.test_hs_upper_code, self.test_hs_middle_code], axis = 1)
         print(train_np.shape,test_np.shape)
+
         for i in range(len(self.column_list)):
             
-            self.dictionary[self.column_list[i]] = {}
+            self.dictionary[str(self.column_list[i])] = {}
             concat = np.concatenate([train_np[:, i], test_np[:, i]], axis = 0)
             total_code, total_count = np.unique(concat, return_counts=True)
-            crime_code, crime_count = np.unique(train_np[:, i][self.crime_idx], return_counts=True)
-            crime_ratio = np.empty((total_count.shape[0], ))
+            train_code, _ = np.unique(train_np[:, i], return_counts = True)
+            test_code, _ = np.unique(test_np[:, i], return_counts = True)
+            train_crime_code, train_crime_count = np.unique(train_np[:, i][self.crime_idx], return_counts=True)
 
-            c_idx = 0
+            n_ratio = 0.
+
             for assign_idx, c in enumerate(total_code) : 
-                if c not in crime_code : 
-                    crime_ratio[assign_idx] = 0.
-                else :
-                    crime_ratio[assign_idx] = np.round(crime_count[c_idx] / total_count[assign_idx], 4)
-                    c_idx += 1
-
-                self.dictionary[self.column_list[i]][c] = {}
-                self.dictionary[self.column_list[i]][c]['count'] = int(total_count[assign_idx])
-                self.dictionary[self.column_list[i]][c]['ratio'] = float(crime_ratio[assign_idx])
-                self.dictionary[self.column_list[i]][c]['onehot'] = int(assign_idx)
                 
+                # Make key, value pair in dictionary
+                self.dictionary[self.column_list[i]][c] = {}
+
+                # In case of, code is only in test.csv
+                if c not in train_code : 
+                    self.dictionary[self.column_list[i]][c]['count'] = int(0)
+                    self.dictionary[self.column_list[i]][c]['ratio'] = float(0.)
+                    self.dictionary[self.column_list[i]][c]['is_mask'] = True
+
+                # In case of, code is only in test.csv
+                elif c not in test_code :
+                    self.dictionary[self.column_list[i]][c]['count'] = int(0)
+                    self.dictionary[self.column_list[i]][c]['ratio'] = float(0.)
+                    self.dictionary[self.column_list[i]][c]['is_mask'] = True
+
+                # In case of, code is in train.csv
+                else :
+                    total_c = total_count[assign_idx]
+                    where_code = np.where(train_crime_code == c)[0]
+                    if len(where_code) ==  0: 
+                        crime_c = 0
+                        n_ratio = 0.
+                    else :
+                        crime_c = train_crime_count[where_code][0]
+                        n_ratio = (crime_c / total_c)
+
+                    # Compute absolute substraction of crime-proportion and standard
+                    proportion = np.abs((crime_c / total_c) - self.crime_proportion)
+
+                    if proportion < 0.05 :
+                        self.dictionary[self.column_list[i]][c]['count'] = int(0)
+                        self.dictionary[self.column_list[i]][c]['ratio'] = float(0.)
+                        self.dictionary[self.column_list[i]][c]['is_mask'] = True
+
+                    else :
+                        self.dictionary[self.column_list[i]][c]['count'] = int(crime_c)
+                        self.dictionary[self.column_list[i]][c]['ratio'] = float(np.round(n_ratio, 6))
+                        self.dictionary[self.column_list[i]][c]['is_mask'] = False
+
+                    # Exeptional condition for error cases
+                    if total_code[assign_idx] != c : 
+                        print('Caused Error!')
+                        sys.exit()
+        
         return self.dictionary
 
 # Example
