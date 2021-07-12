fix the duplicate rescale for numerical data

DataProcessing/preprocessing.py
@@ -33,7 +33,7 @@ class RescaleNumeric:
     def __init__(self):
         self.minmax_scale=preprocessing.MinMaxScaler(feature_range=(0,1))
     def __call__(self,x):
-        x_scale=self.minmax_scale.fit_transform(np.log(x+1))
+        x_scale=self.minmax_scale.fit_transform(np.log(x+1e-7).reshape(-1,1))
         return x_scale
 
 
@@ -99,7 +99,7 @@ class Preprocessing:
         numeric_features = ['신고중량(KG)', '과세가격원화금액', '관세율']
         dataframe.fillna('Missing', inplace=True)
         for column in numeric_features:
-            dataframe[column] = rescaler(np.log(dataframe.pop(column).to_numpy()+1).reshape(-1,1))
+            dataframe[column] = rescaler(dataframe.pop(column).to_numpy())
         np_data = dataframe[['신고중량(KG)', '과세가격원화금액','관세율']].to_numpy()
         np_data[:,0] = np_data[:,0]/(np_data[:,0].max()+1e-10)
         np_data[:,1] = np_data[:,1]/(np_data[:,1].max()+1e-10)
@@ -168,12 +168,11 @@ class Preprocessing:
         np_data[:,2] = np_data[:,2]/(np_data[:,2].max()+1e-10)
         dataframe['HS_upper'] = dataframe['HS10단위부호'] // 100000000 # 위 2자리
         dataframe['HS_middle'] = dataframe['HS10단위부호'] // 1000000 # 위 4자리
-        dataframe['HS_low'] = dataframe['HS10단위부호'] // 10000 # 위 4자리
+        dataframe['HS_low'] = dataframe['HS10단위부호'] // 10000 # 위 6자리
         dataframe['관세율구분코드_1자리']=dataframe['관세율구분코드'].str.slice(start = 0, stop = 1)
 
         dataframe.drop(['신고일자','신고번호','우범여부','핵심적발'],axis=1,inplace=True,errors='ignore')#,'HS10단위부호'
         len_df = len(dataframe.index)
-
         add_count_ratio_list=['crime_count','crime_ratio','priority_ratio']
         reg_count_ratio_list=['crime_count']
 

Model/indmodel.py
@@ -62,15 +62,6 @@ class PriorityModel(nn.Module):
     def __init__(self,input_space,output_space,configs):
         super(PriorityModel,self).__init__()
         self.model=nn.Sequential(
-            nn.Linear(input_space,500),
-            nn.BatchNorm1d(500),
-            nn.LeakyReLU(),
-            nn.Linear(500,500),
-            nn.BatchNorm1d(500),
-            nn.LeakyReLU(),
-            nn.Linear(500,output_space),
-        )
-        """
             nn.Linear(input_space,5000),
             nn.BatchNorm1d(5000),
             nn.LeakyReLU(),
@@ -81,14 +72,14 @@ class PriorityModel(nn.Module):
             nn.BatchNorm1d(100),
             nn.LeakyReLU(),
             nn.Linear(100,output_space),
-        """
+        )
         self.criterion=nn.CrossEntropyLoss()
         self.optimizer=torch.optim.Adam(self.model.parameters(),lr=configs['lr'],weight_decay=configs['weight_decay'])
         self.scheduler=torch.optim.lr_scheduler.StepLR(optimizer=self.optimizer,step_size=configs['lr_decay'], gamma=configs['lr_decay_rate'])
 
         for m in self.modules():
             if isinstance(m,(nn.Linear)):
-                nn.init.kaiming_uniform_(m.weight,nonlinearity='relu')
+                nn.init.kaiming_uniform_(m.weight,nonlinearity='relu') # leaky
             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)