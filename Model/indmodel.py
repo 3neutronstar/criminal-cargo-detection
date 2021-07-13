0661cf8f99b5c1923b06925b8e91f005ecbaa5ca
fabiano77 <fabiano7@naver.com>
2021-07-13 21:28:56 +0900

    Modify model parameters: Add weight & drop out

--- Model/indmodel.py
+++ Model/indmodel.py
@@ -5,33 +5,26 @@ class CrimeModel(nn.Module):
     def __init__(self,input_space,output_space,configs):
         super(CrimeModel,self).__init__()
         self.model=nn.Sequential(
-            nn.Linear(input_space,5000),
-            nn.BatchNorm1d(5000),
-            nn.ReLU(),
-            nn.Linear(5000,1000),
+            nn.Linear(input_space,6000),
+            nn.BatchNorm1d(6000),#5000),
+            nn.ELU(),
+            nn.Linear(6000,3000),
+            nn.BatchNorm1d(3000),
+            nn.ELU(),
+            nn.Dropout(0.5),
+            nn.Linear(3000,1000),
             nn.BatchNorm1d(1000),
-            nn.ReLU(),
-            nn.Linear(1000,100),
-            nn.BatchNorm1d(100),
-            nn.ReLU(),
-            nn.Linear(100,output_space),         
+            nn.ELU(),
+            nn.Dropout(0.5),
+            nn.Linear(1000,450),
+            nn.BatchNorm1d(450),
+            nn.ELU(),
+            nn.Dropout(0.5),
+            nn.Linear(450,output_space)
         )
-        """
-            nn.Linear(input_space,5000),
-            nn.BatchNorm1d(5000),
-            nn.ReLU(),
-
-            nn.Linear(5000,1000),
-            nn.BatchNorm1d(1000),
-            nn.ReLU(),
-
-            nn.Linear(1000,100),
-            nn.BatchNorm1d(100),
-            nn.ReLU(),
-
-            nn.Linear(100,output_space)
-        """
-        self.criterion=nn.CrossEntropyLoss()
+        self.weight1 = torch.tensor([0.2988, 1.0])
+        self.criterion=nn.CrossEntropyLoss(weight=self.weight1)
+        #self.criterion=nn.CrossEntropyLoss()
         # self.criterion=CrossEntropyLoss_weighted()
         # self.criterion=FocalLoss(gamma=0)
         self.optimizer=torch.optim.Adam(self.model.parameters(),lr=configs['lr'],weight_decay=configs['weight_decay'])
@@ -63,24 +56,32 @@ class PriorityModel(nn.Module):
     def __init__(self,input_space,output_space,configs):
         super(PriorityModel,self).__init__()
         self.model=nn.Sequential(
-            nn.Linear(input_space,5000),
-            nn.BatchNorm1d(5000),
-            nn.ReLU(),
-            nn.Linear(5000,1000),
+            nn.Linear(input_space,6000),
+            nn.BatchNorm1d(6000),#5000),
+            nn.ELU(),
+            nn.Linear(6000,3000),
+            nn.BatchNorm1d(3000),
+            nn.ELU(),
+            nn.Dropout(0.5),
+            nn.Linear(3000,1000),
             nn.BatchNorm1d(1000),
-            nn.ReLU(),
-            nn.Linear(1000,100),
-            nn.BatchNorm1d(100),
-            nn.ReLU(),
-            nn.Linear(100,output_space),
+            nn.ELU(),
+            nn.Dropout(0.5),
+            nn.Linear(1000,450),
+            nn.BatchNorm1d(450),
+            nn.ELU(),
+            nn.Dropout(0.5),
+            nn.Linear(450,output_space)
         )
-        self.criterion=nn.CrossEntropyLoss()
+        self.weight2 = torch.tensor([1.0, 0.9431])
+        self.criterion=nn.CrossEntropyLoss(weight=self.weight2)
+        #self.criterion=nn.CrossEntropyLoss()
         self.optimizer=torch.optim.Adam(self.model.parameters(),lr=configs['lr'],weight_decay=configs['weight_decay'])
         self.scheduler=torch.optim.lr_scheduler.StepLR(optimizer=self.optimizer,step_size=configs['lr_decay'], gamma=configs['lr_decay_rate'])
 
         for m in self.modules():
             if isinstance(m,(nn.Linear)):
-                nn.init.kaiming_uniform_(m.weight,nonlinearity='relu') # leaky
+                nn.init.kaiming_uniform_(m.weight,nonlinearity='relu')
             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
