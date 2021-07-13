25f07f3371e13d583e8aecbb8ec0a267f8700134
ykykyk112 <ykykyk112@naver.com>
2021-07-13 20:36:09 +0900

    2021.07.13 Add masking in dict

--- DataProcessing/make_dict.py
+++ DataProcessing/make_dict.py
@@ -21,7 +21,7 @@ class MappingJsonGenerator():
         self.train_csv['관세율구분코드_1자리']=self.train_csv['관세율구분코드'].str.slice(start = 0, stop = 1)
         # self.test_csv['관세율구분코드_1자리']=self.test_csv['관세율구분코드'].str.slice(start = 0, stop = 1)
 
-        self.train_csv['단위무게(KG)가격'] = (self.train_csv['과세가격원화금액']/self.train_csv['신고중량(KG)']).map(lambda x: np.round(x, 7)).map(str)
+        self.train_csv['단위무게(KG)가격'] = (self.train_csv['과세가격원화금액']/self.train_csv['신고중량(KG)']).map(lambda x: np.round(x, 0)).map(str)
         # self.test_csv['단위무게(KG)가격'] = (self.test_csv['과세가격원화금액']/self.test_csv['신고중량(KG)']).map(lambda x: np.round(x, 7)).map(str)
 
         self.train_csv.drop(['과세가격원화금액', '신고중량(KG)'], axis = 1,errors='ignore',inplace=True)
@@ -55,6 +55,12 @@ class MappingJsonGenerator():
         self.non_priority_idx = np.where(self.priority == 1)[0]
         self.priority_idx = np.where(self.priority == 2)[0]
         self.dictionary = dict()
+
+        upper_limit = 0
+        #self.crime_threshold_no_valid = [150, 7, 3, 3, 40, upper_limit, upper_limit, upper_limit, upper_limit, upper_limit, upper_limit, 20, 10, upper_limit, upper_limit, upper_limit, 30, 30, 20, upper_limit, 3]
+        #self.priority_threshold_no_valid = [150, 16, 3, 3, 60, upper_limit, upper_limit, upper_limit, upper_limit, upper_limit, upper_limit, 30, 15, upper_limit, upper_limit, upper_limit, 30, 25, 15, upper_limit, 3]
+        self.crime_threshold_no_valid = [0]*21
+        self.priority_threshold_no_valid = [0]*21
         
     def __call__(self):
         return self.forward()
@@ -79,23 +85,24 @@ class MappingJsonGenerator():
             total_code, total_count = np.unique(concat, return_counts=True)
             crime_code, crime_count = np.unique(train_np[:, i][self.crime_idx], return_counts=True)
 
-            #--------------------------------------------
-            crime_dict = dict(zip(crime_code, crime_count))
-            crime_dict = np.array(sorted(crime_dict.items(), reverse=True, key = lambda x : x[1]))
-            #--------------------------------------------
-
             _, total_priority_count = np.unique(np.concatenate((train_np[:, i][self.priority_idx],train_np[:, i][self.non_priority_idx]),axis=0), return_counts=True)
             priority_code, priority_count = np.unique(train_np[:, i][self.priority_idx], return_counts=True)
             
-            font_path = "C:\\Windows\\Fonts\\gulim.ttc"
-            font = font_manager.FontProperties(fname=font_path).get_name()
-            rc('font', family=font)
+            #print(col)
+            #--------------------------------------------
+            # crime_dict = dict(zip(priority_code, priority_count))
+            # crime_dict = np.array(sorted(crime_dict.items(), reverse=True, key = lambda x : x[1]))
+            # #--------------------------------------------
 
-            plt.plot(crime_dict[:, 0], crime_dict[:, 1].astype(np.int))
-            plt.title(col+'/'+str(crime_dict[:, 0].shape[0]), fontsize = 20)
-            ax = plt.gca()
-            ax.axes.xaxis.set_visible(False)
-            plt.show()
+            # font_path = "C:\\Windows\\Fonts\\gulim.ttc"
+            # font = font_manager.FontProperties(fname=font_path).get_name()
+            # rc('font', family=font)
+
+            # plt.plot(crime_dict[:, 0], crime_dict[:, 1].astype(np.int))
+            # plt.title(col+'/'+str(crime_dict[:, 0].shape[0]), fontsize = 20)
+            # ax = plt.gca()
+            # ax.axes.xaxis.set_visible(False)
+            # plt.show()
 
             crime_ratio = np.empty((total_count.shape[0], ))
             
@@ -110,11 +117,16 @@ class MappingJsonGenerator():
                 if c not in crime_code : 
                     crime_ratio[assign_idx] = 0.
                     self.dictionary[col][c]['crime_count'] = int(0)
+                    self.dictionary[col][c]['is_mask'] = True
                 else :
                     crime_ratio[assign_idx] = np.round(crime_count[c_idx] / total_count[assign_idx], 4)
                     self.dictionary[col][c]['crime_count'] = int(crime_count[c_idx])
                     crime_count_mean += float(crime_count[c_idx])
                     crime_ratio_mean += float(crime_ratio[assign_idx]*crime_count[c_idx])
+                    if crime_count[c_idx] <= self.crime_threshold_no_valid[i]:
+                        self.dictionary[col][c]['is_mask'] = True
+                    else : 
+                        self.dictionary[col][c]['is_mask'] = False
                     c_idx += 1
 
                 self.dictionary[col][c]['crime_ratio'] = float(crime_ratio[assign_idx])
@@ -123,8 +135,8 @@ class MappingJsonGenerator():
             crime_concat = np.concatenate([crime_ratio.reshape(-1, 1), total_code.reshape(-1, 1)], axis = 1)
             crime_concat = np.array(sorted(crime_concat, key = lambda x : x[0], reverse=True))
 
-            for i, c in enumerate(crime_concat[:, 1]):
-                self.dictionary[col][c]['sorted_crime_onehot'] = int(i+1)
+            for k, c in enumerate(crime_concat[:, 1]):
+                self.dictionary[col][c]['sorted_crime_onehot'] = int(k+1)
 
 
                 
@@ -132,6 +144,7 @@ class MappingJsonGenerator():
                 if p not in priority_code: 
                     priority_ratio[assign_idx] = 0.
                     self.dictionary[col][p]['priority_count'] = int(0)
+                    self.dictionary[col][p]['is_mask'] = True
                 else :
                     assign_priority_idx=np.where(priority_code==p)[0]
                     #priority_ratio[assign_idx] = np.round(priority_count[p_idx] / total_priority_count[assign_priority_idx], 4)
@@ -139,6 +152,10 @@ class MappingJsonGenerator():
                     self.dictionary[col][p]['priority_count'] = int(priority_count[p_idx])
                     priority_count_mean += float(priority_count[p_idx])
                     priority_ratio_mean += float(priority_ratio[assign_idx]*priority_count[p_idx])
+                    if priority_count[p_idx] <= self.priority_threshold_no_valid[i]:
+                        self.dictionary[col][p]['is_mask'] = True
+                    else : 
+                        self.dictionary[col][p]['is_mask'] = False
                     p_idx += 1
 
                 
@@ -147,8 +164,8 @@ class MappingJsonGenerator():
             priority_concat = np.concatenate([priority_ratio.reshape(-1, 1), total_code.reshape(-1, 1)], axis = 1)
             priority_concat = np.array(sorted(priority_concat, key = lambda x : x[0], reverse=True))
 
-            for i, c in enumerate(priority_concat[:, 1]):
-                self.dictionary[col][c]['sorted_priority_onehot'] = int(i+1)
+            for k, c in enumerate(priority_concat[:, 1]):
+                self.dictionary[col][c]['sorted_priority_onehot'] = int(k+1)
 
             self.dictionary[col]['crime_count_mean'] = crime_count_mean / train_valid_np.shape[0]
             self.dictionary[col]['crime_ratio_mean'] =crime_ratio_mean / train_valid_np.shape[0]
