84aa5a7e3a19b0d8ea46c92b95cfa5d9125fcf4d
fabiano77 <fabiano7@naver.com>
2021-07-16 16:03:44 +0900

    Fix error

--- Utils/record.py
+++ Utils/record.py
@@ -101,8 +101,11 @@ class RecordData:
             crime_dict=torch.load(os.path.join(self.save_path,self.configs['file_name'],'best_crime_model.pt'))
             priority_dict=torch.load(os.path.join(self.save_path,self.configs['file_name'],'best_priority_model.pt'))
             print("========== Performances ==========")
-            print("crime F1: {:.3f} crime Acc: {:.3f}".format(crime_dict['f1score'],crime_dict['accuracy']))
-            print("priority F1: {:.3f} priority Acc: {:.3f}".format(priority_dict['f1score'],priority_dict['accuracy']))
+            print(crime_dict['crime']['f1score'])
+            # print("crime F1: {:.3f} crime Acc: {:.3f}".format(crime_dict['f1score'],crime_dict['accuracy']))
+            # print("priority F1: {:.3f} priority Acc: {:.3f}".format(priority_dict['f1score'],priority_dict['accuracy']))
+            print("crime F1: {:.3f} crime Acc: {:.3f}".format(crime_dict['crime']['f1score'],crime_dict['crime']['accuracy']))
+            print("priority F1: {:.3f} priority Acc: {:.3f}".format(priority_dict['priority']['f1score'],priority_dict['priority']['accuracy']))
             print("==================================")
             dict_model={**crime_dict,**priority_dict}
             self.model.load_model(dict_model)
